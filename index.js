d9a7a14cf7914985676e80c6a3ca80eac50d94ce
devarjankars <devarjankars@gmail.com>
2024-11-21 15:24:06 +0530

    learning Gsap animation

---
+++ index.js
@@ -0,0 +1,32 @@
+gsap.from("#img1",{
+
+    delay:0.5,
+    opacity:0,
+    duration:1,
+    y:60,
+    
+})
+gsap.from("#img2",{
+
+    delay:0.5,
+    opacity:0,
+    duration:1,
+    x:60,
+    
+})
+gsap.from("#img3",{
+
+    delay:0.5,
+    opacity:0,
+    duration:1,
+    x:-60,
+    
+})
+gsap.from("#main-heading",{
+
+    delay:0.1,
+    opacity:0,
+    duration:1,
+    y:100,
+    
+})
